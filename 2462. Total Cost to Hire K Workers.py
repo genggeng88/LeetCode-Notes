9bd442a237bf31e933f96cdb0029469b89574e76
Qin Geng <gengqin50@gmail.com>
2024-05-22 09:03:22 -0700

    05/22

---
+++ 2462. Total Cost to Hire K Workers.py
@@ -0,0 +1,25 @@
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        head_workers = costs[:candidates]
+        tail_workers = costs[max(candidates, len(costs)-candidates):]
+        heapify(head_workers)
+        heapify(tail_workers)
+
+        next_head = candidates
+        next_tail = len(costs) - candidates - 1
+        ans = 0
+
+        for _ in range(k):
+            if not tail_workers or head_workers and head_workers[0] <= tail_workers[0]:
+                ans += heappop(head_workers)
+                if next_head <= next_tail:
+                    heappush(head_workers, costs[next_head])
+                    next_head += 1
+            
+            else:
+                ans += heappop(tail_workers)
+                if next_head <= next_tail:
+                    heappush(tail_workers, costs[next_tail])
+                    next_tail -= 1
+            
+        return ans
