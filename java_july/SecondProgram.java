8c960b5af27cb6bcf88db2003add6f54479f5d80
Qin Geng <gengqin50@gmail.com>
2024-07-19 14:58:32 -0700

    07/19

---
+++ java_july/SecondProgram.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+
+public class SecondProgram {
+    public static void main ( String args[] ) { if (args.length > 0) {
+        System.out.println("Hello " + args[0]); } else {
+        System.out.println( "Hello everybody!"); }
+} }
