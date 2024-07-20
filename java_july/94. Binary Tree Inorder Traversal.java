05f4c263031658de30ead1d4f490ad86ec555889
Qin Geng <gengqin50@gmail.com>
2024-07-20 08:04:24 -0700

    07/20

---
+++ java_july/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.add(curr);
+                curr = curr.left;
+            }
+
+            TreeNode tmp = stack.pop();
+            res.add(tmp.val);
+            curr = tmp.right;
+        } 
+        return res;
+    }
+}
